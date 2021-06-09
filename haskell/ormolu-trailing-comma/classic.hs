10796ab45e09c34b2ecb4b36368a4a4edc0f0a99
Alexey Kuleshevich <alexey@kuleshevi.ch>
2021-06-09 12:16:31 +0300

    Commit initial formatting commit

---
+++ haskell/ormolu-trailing-comma/classic.hs
@@ -0,0 +1,36 @@
+
+lists :: [String]
+lists =
+  [ "It is often lists do not fit into a single line."
+  , "We need to format them in a readable fashion"
+  , "without compromising functionality."
+  ]
+
+data Record =
+  Record
+    { records :: String
+    , are :: Bool
+    , no :: Bool
+    }
+
+myRecord :: Record
+myRecord =
+  Record
+    { records = "Records are also suscpetible to trailing comma problem"
+    , are = True
+    , no = False
+    }
+
+
+data VeryLongTypeNameThatCausesForTupleiInTheTypeSignatureToWrapAroundTooAmIRight = AmIRight
+
+tuples ::
+     ( String
+     , String
+     , VeryLongTypeNameThatCausesForTupleiInTheTypeSignatureToWrapAroundTooAmIRight
+     )
+tuples =
+  ( "It is less common for tuples to not fit into a single line."
+  , "However it does still happen"
+  , AmIRight
+  )
---
+++ haskell/ormolu-trailing-comma/ormolu.hs
@@ -0,0 +1,34 @@
+
+lists :: [String]
+lists =
+  [ "It is often that lists do not fit into a single line.",
+    "We need to format them in a readable fashion",
+    "without compromising functionality."
+  ]
+
+data Record = Record
+  { records :: String,
+    are :: Bool,
+    no :: Bool
+  }
+
+myRecord :: Record
+myRecord =
+  Record
+    { records = "Records are aslo suscpetible to trailing comma problem",
+      are = True,
+      no = False
+    }
+
+data VeryLongTypeNameThatCausesForTupleiInTheTypeSignatureToWrapAroundTooAmIRight = AmIRight
+
+tuples ::
+  ( String,
+    String,
+    VeryLongTypeNameThatCausesForTupleiInTheTypeSignatureToWrapAroundTooAmIRight
+  )
+tuples =
+  ( "It is less common for tuples to not fit into a single line.",
+    "However it does still happen",
+    AmIRight
+  )
